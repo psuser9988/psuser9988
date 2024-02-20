a919492d31538fdfec873dfa3f9c0b4ed87d049a
pkellner <peter@peterkellner.net>
2024-02-19 16:52:27 -0800

    go2

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https:/psuser9988.peterkellner.net/", // replace this with your deployed domain
   author: "Sat Naing",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "AstroPaper",
